ecf4aa736a294cbe0ac2ae15ed2d7aa24e823433
pajkungen <skarec@gmail.com>
2021-11-10 12:38:10 +0100

    added client.sh

---
+++ client.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+######################################
+# Client.sh
+# Author: Mattias Skarec             #
+# 2021                               #
+#                                    #
+# The client part of the chat client #
+#                                    #
+######################################
+
+#Inputs
+
+login() {
+read -p "Username:" username
+read -p "What room would you like to join?" room
+}
+
+
